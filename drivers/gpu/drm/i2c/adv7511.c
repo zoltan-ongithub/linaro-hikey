d23fc0763a673882c7e9749e0e549ca275c6857e
John Stultz <john.stultz@linaro.org>
2015-12-15 17:11:37 -0800

    adv7511: Fix use of drm_display_mode pointer after free
    The adv7511 structure uses a drm_display_mode pointer,
    which is assigned to a value allocated higher in the
    call stack that is later destryed.
    
    This results in the adv7511 initializing occasionally
    with garbage values.
    
    Fix this issue by using a drm_disply_mode structure
    instead of a pointer in the adv7511 structure, and
    copying the provided structure over entirely.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/gpu/drm/i2c/adv7511.c
+++ drivers/gpu/drm/i2c/adv7511.c
@@ -374,7 +374,7 @@ static void adv7511_set_link_config(struct adv7511 *adv7511,
 
 static void adv7511_dsi_config_tgen(struct adv7511 *adv7511)
 {
-	struct drm_display_mode *mode = adv7511->curr_mode;
+	struct drm_display_mode *mode = &adv7511->curr_mode;
 	unsigned int hsw, hfp, hbp, vsw, vfp, vbp, hd, vd;
 
 	hsw = mode->hsync_end - mode->hsync_start;
@@ -856,7 +856,7 @@ static void adv7511_mode_set(struct adv7511 *adv7511,
 	regmap_update_bits(adv7511->regmap, 0x17,
 		0x60, (vsync_polarity << 6) | (hsync_polarity << 5));
 
-	adv7511->curr_mode = adj_mode;
+	memcpy(&adv7511->curr_mode, adj_mode, sizeof(adv7511->curr_mode));
 	/*
 	 * TODO Test first order 4:2:2 to 4:4:4 up conversion method, which is
 	 * supposed to give better results.
